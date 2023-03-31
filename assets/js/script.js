d3170f4d999c50ed9b24bf93d14326260d7b6be1
Ian Bui <thinkbui@gmail.com>
2023-03-30 22:28:46 -0500

    Add prompts for types

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,5 +10,47 @@ function writePassword() {
 
 }
 
+function generatePassword() {
+  var optionString = "";
+  optionString += confirmLowerCase();
+  optionString += confirmUpperCase();
+  optionString += confirmNumber();
+  optionString += confirmSpecial();
+
+  return optionString;
+}
+
+function confirmLowerCase() {
+  var userResponse = confirm("Use lower case letters?");
+  if (userResponse) {
+    return "abcdefghijklmnopqrstuvwxyz";
+  }
+  return "";
+}
+
+function confirmUpperCase() {
+  var userResponse = confirm("Use upper case letters?");
+  if (userResponse) {
+    return "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  }
+  return "";
+}
+
+function confirmNumber() {
+  var userResponse = confirm("Use numeric characters?");
+  if (userResponse) {
+    return "0123456789";
+  }
+  return "";
+}
+
+function confirmSpecial() {
+  var userResponse = confirm("Use special characters?");
+  if (userResponse) {
+    return "~!@#$%^&*?+=-_";
+  }
+  return "";
+}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
