92f60e9ad1a94df60f61cc19e1f35acd8488434c
Ian Bui <thinkbui@gmail.com>
2023-03-31 18:35:57 -0500

    Added guarantee of each type of character and built function for length prompt

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,12 +11,15 @@ function writePassword() {
 }
 
 function generatePassword() {
-  var optionString = confirmType(lowerCasePrompt, lowerCaseOptions)
-                     + confirmType(upperCasePrompt, upperCaseOptions)
-                     + confirmType(numberPrompt, numberOptions)
-                     + confirmType(specialPrompt, specialOptions);
-
-  return optionString;
+  var optionString = "";
+  var guaranteedString = "";
+  for (i=0;i<promptData.length;i++){
+    data = confirmType(promptData[i][0],promptData[i][1]);
+    guaranteedString += data[0];
+    optionString += data[1];
+  }
+  var passwordLength = promptLength();
+  return passwordLength;
 }
 
 const lowerCaseOptions = "abcdefghijklmnopqrstuvwxyz";
@@ -29,12 +32,23 @@ const upperCasePrompt = "upper case letters";
 const numberPrompt = "numeric characters";
 const specialPrompt = "special characters";
 
+const promptData = [[lowerCasePrompt,lowerCaseOptions],[upperCasePrompt,upperCaseOptions],[numberPrompt,numberOptions],[specialPrompt,specialOptions]];
+
 function confirmType(message, opts_list) {
   var userResponse = confirm(`Use ${message}?`);
   if (userResponse) {
-    return opts_list
+    var guaranteed_char = opts_list.charAt(Math.floor(Math.random() * opts_list.length));
+    return [guaranteed_char,opts_list];
   }
-  return "";
+  return ["",""];
+}
+
+function promptLength() {
+  var userResponse = parseInt(prompt("How long should the password be? (8-128 characters)"), 10);
+  while (isNaN(userResponse) || userResponse < 8 || userResponse > 128) {
+    userResponse = parseInt(prompt("INVALID ENTRY: Try again.  How long should the password be? (8-128 characters)"), 10);
+  }
+  return userResponse;
 }
 
 // Add event listener to generate button
